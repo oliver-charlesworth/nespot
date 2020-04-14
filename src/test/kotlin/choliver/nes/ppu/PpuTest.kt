fe650fc38d8e56a22f187ca1c475d332ad35fac6
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-04-14 22:34:32 +0100

    Add basic PPU memory test cases

---
+++ src/test/kotlin/choliver/nes/ppu/PpuTest.kt
@@ -0,0 +1,130 @@
+package choliver.nes.ppu
+
+import choliver.nes.Address
+import choliver.nes.Memory
+import choliver.nes.hi
+import choliver.nes.lo
+import choliver.nes.ppu.Ppu.Companion.BASE_PALETTE
+import choliver.nes.ppu.Ppu.Companion.REG_OAMADDR
+import choliver.nes.ppu.Ppu.Companion.REG_OAMDATA
+import choliver.nes.ppu.Ppu.Companion.REG_PPUADDR
+import choliver.nes.ppu.Ppu.Companion.REG_PPUDATA
+import choliver.nes.ppu.Ppu.Companion.REG_PPUSTATUS
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class PpuTest {
+  private val memory = mock<Memory>()
+  private val ppu = Ppu(memory = memory)
+
+  @Nested
+  inner class ExternalMemory {
+    @Test
+    fun `writes to incrementing memory locations`() {
+      setPpuAddress(0x1230)
+
+      ppu.writeReg(REG_PPUDATA, 0x20)
+      ppu.writeReg(REG_PPUDATA, 0x30)
+      ppu.writeReg(REG_PPUDATA, 0x40)
+
+      verify(memory).store(0x1230, 0x20)
+      verify(memory).store(0x1231, 0x30)
+      verify(memory).store(0x1232, 0x40)
+    }
+
+    @Test
+    fun `reads from incrementing memory locations, first read is garbage`() {
+      whenever(memory.load(0x1230)) doReturn 0x20
+      whenever(memory.load(0x1231)) doReturn 0x30
+      whenever(memory.load(0x1232)) doReturn 0x40
+
+      setPpuAddress(0x1230)
+
+      ppu.readReg(REG_PPUDATA)  // Ignore garbage read
+      assertEquals(0x20, ppu.readReg(REG_PPUDATA))
+      assertEquals(0x30, ppu.readReg(REG_PPUDATA))
+      assertEquals(0x40, ppu.readReg(REG_PPUDATA))
+    }
+
+    @Test
+    fun `reads from successive latched addresses, first read is garbage in each case`() {
+      whenever(memory.load(0x1230)) doReturn 0x20
+      whenever(memory.load(0x1590)) doReturn 0x30
+
+      setPpuAddress(0x1230)
+
+      ppu.readReg(REG_PPUDATA)  // Ignore garbage read
+      assertEquals(0x20, ppu.readReg(REG_PPUDATA))
+
+      setPpuAddress(0x1590)
+
+      ppu.readReg(REG_PPUDATA)  // Ignore garbage read
+      assertEquals(0x30, ppu.readReg(REG_PPUDATA))
+    }
+  }
+
+  @Nested
+  inner class PaletteMemory {
+    @Test
+    fun `writes and reads from incrementing palette memory locations, without garbage read`() {
+      setPpuAddress(BASE_PALETTE)
+
+      ppu.writeReg(REG_PPUDATA, 0x20)
+      ppu.writeReg(REG_PPUDATA, 0x30)
+      ppu.writeReg(REG_PPUDATA, 0x40)
+
+      setPpuAddress(BASE_PALETTE)
+
+      assertEquals(0x20, ppu.readReg(REG_PPUDATA))
+      assertEquals(0x30, ppu.readReg(REG_PPUDATA))
+      assertEquals(0x40, ppu.readReg(REG_PPUDATA))
+    }
+
+    @Test
+    fun `writes and reads from mirrors`() {
+      setPpuAddress(BASE_PALETTE)
+      ppu.writeReg(REG_PPUDATA, 0x20)
+      setPpuAddress(BASE_PALETTE + 0x1F)
+      ppu.writeReg(REG_PPUDATA, 0x30)
+
+      // First mirror
+      setPpuAddress(BASE_PALETTE + 0x20)
+      assertEquals(0x20, ppu.readReg(REG_PPUDATA))
+
+      // Last mirror
+      setPpuAddress(BASE_PALETTE + 0xFF)
+      assertEquals(0x30, ppu.readReg(REG_PPUDATA))
+    }
+  }
+
+  @Nested
+  inner class OamMemory {
+    @Test
+    fun `writes and reads from incrementing memory locations`() {
+      ppu.writeReg(REG_OAMADDR, 0x50)
+
+      ppu.writeReg(REG_OAMDATA, 0x20)
+      ppu.writeReg(REG_OAMDATA, 0x30)
+      ppu.writeReg(REG_OAMDATA, 0x40)
+
+      ppu.writeReg(REG_OAMADDR, 0x50)
+
+      assertEquals(0x20, ppu.readReg(REG_OAMDATA))
+      assertEquals(0x30, ppu.readReg(REG_OAMDATA))
+      assertEquals(0x40, ppu.readReg(REG_OAMDATA))
+    }
+  }
+
+  private fun setPpuAddress(addr: Address) {
+    ppu.readReg(REG_PPUSTATUS)  // Reset address latch (whatever that means)
+    ppu.writeReg(REG_PPUADDR, addr.hi())
+    ppu.writeReg(REG_PPUADDR, addr.lo())
+  }
+
+
+}

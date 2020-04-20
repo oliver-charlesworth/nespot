c8c6335f9b4d176d5cc4eba58e6b8a2e59d370dc
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-04-20 23:27:43 +0100

    Optimise Palette

--- src/main/kotlin/choliver/nes/ppu/Palette.kt
+++ src/main/kotlin/choliver/nes/ppu/Palette.kt
@@ -3,17 +3,16 @@ package choliver.nes.ppu
 import choliver.nes.Address
 import choliver.nes.Data
 import choliver.nes.Memory
-import choliver.nes.data
 
 class Palette : Memory {
-  private val raw = ByteArray(32) { 0x00.toByte() }
+  private val raw = IntArray(32) { 0x00 } // Not bytes, to avoid conversion overhead
 
-  override fun load(addr: Address): Data = raw[map(addr and 31)].data()
+  override fun load(addr: Address): Data = raw[mapMirrors(addr)]
 
   override fun store(addr: Address, data: Data) {
-    raw[map(addr and 31)] = data.toByte()
+    raw[mapMirrors(addr )] = data
   }
 
   // See http://wiki.nesdev.com/w/index.php/PPU_palettes#Memory_Map
-  private fun map(addr: Address) = if (addr in listOf(0x10, 0x14, 0x18, 0x1C)) (addr - 0x10) else addr
+  private fun mapMirrors(addr: Address) = if ((addr and 0x03) == 0) (addr and 0xEF) else addr
 }

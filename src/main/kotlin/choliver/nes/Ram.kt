e62818130c5ec51432291efa19e52f62d22e253c
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-04-20 23:56:01 +0100

    Switch Ram to IntArray

--- src/main/kotlin/choliver/nes/Ram.kt
+++ src/main/kotlin/choliver/nes/Ram.kt
@@ -2,11 +2,11 @@ package choliver.nes
 
 // TODO - power of two for efficiency?  (i.e. bitmask rather than modulo)
 class Ram(size: Int) : Memory {
-  private val raw = ByteArray(size) { 0xCC.toByte() }
+  private val raw = IntArray(size) { 0xCC } // Not bytes, to avoid conversion overhead
 
-  override fun load(addr: Address) = raw[addr].data()
+  override fun load(addr: Address): Data = raw[addr]
 
   override fun store(addr: Address, data: Data) {
-    raw[addr] = data.toByte()
+    raw[addr] = data
   }
 }

853208d6a0df4e1649415c298c7b7e6c46760d8c
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-04-22 11:23:47 +0100

    Add test for FakeJoypads, and fix latching bug (#35)

--- src/main/kotlin/choliver/nespot/runner/FakeJoypads.kt
+++ src/main/kotlin/choliver/nespot/runner/FakeJoypads.kt
@@ -7,23 +7,23 @@ import choliver.nespot.isBitSet
 
 class FakeJoypads : Joypads {
   private val status = mutableMapOf(1 to 0.data(), 2 to 0.data())
-  private val statusLatched = mutableMapOf(1 to 0.data(), 2 to 0.data())
+  private val copied = mutableMapOf(1 to 0.data(), 2 to 0.data())
   private var transparent = false
 
   override fun write(data: Data) {
-    maybeLatch()
     transparent = data.isBitSet(0)
+    maybeCopy()
   }
 
   override fun read1() = read(1)
 
   override fun read2() = read(2)
 
-  private fun read(which: Int): Data {
-    maybeLatch()
-    return (statusLatched[which]!! and 1)
-      .also { statusLatched[which] = statusLatched[which]!! shr 1 }
-  }
+  private fun read(which: Int) = (copied[which]!! and 1)
+    .also {
+      copied[which] = copied[which]!! shr 1
+      maybeCopy()
+    }
 
   fun up(which: Int, button: Joypads.Button) {
     update(which, button, false)
@@ -38,12 +38,13 @@ class FakeJoypads : Joypads {
     val mask = (1 shl shift)
     val s = status[which] ?: throw IllegalArgumentException()  // Should never happen
     status[which] = if (state) (s or mask) else (s and mask.inv())
+    maybeCopy()
   }
 
-  private fun maybeLatch() {
+  private fun maybeCopy() {
     if (transparent) {
-      statusLatched.clear()
-      statusLatched += status
+      copied.clear()
+      copied += status
     }
   }
 }

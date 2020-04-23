7087ea5567368d3e70071d3b24319d9125ae267e
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-04-23 23:31:11 +0100

    Add pulse generators

--- src/main/kotlin/choliver/nespot/apu/Apu.kt
+++ src/main/kotlin/choliver/nespot/apu/Apu.kt
@@ -5,43 +5,56 @@ import choliver.nespot.Data
 class Apu(
   private val buffer: ByteArray
 ) {
+  private val pulse1 = PulseGenerator()
+  private val pulse2 = PulseGenerator()
   private val triangle = TriangleGenerator()
 
   fun writeReg(reg: Int, data: Data) {
     when (reg) {
       REG_SQ1_VOL -> {
-
+        pulse1.dutyCycle = (data and 0xC0) ushr 6
+        // TODO - halt
+        // TODO - volume/envelope flag
+        // TODO - envelope divider
+        pulse1.volume = data and 0x0F
       }
 
       REG_SQ1_SWEEP -> {
-
+        // TODO
       }
 
       REG_SQ1_LO -> {
-
+        pulse1.timer = (pulse1.timer and 0x0700) or data
       }
 
       REG_SQ1_HI -> {
-
+        pulse1.timer = (pulse1.timer and 0x00FF) or ((data and 0x07) shl 8)
+        pulse1.length = (data and 0xF8) ushr 5
       }
 
       REG_SQ2_VOL -> {
-
+        pulse2.dutyCycle = (data and 0xC0) ushr 6
+        // TODO - halt
+        // TODO - volume/envelope flag
+        // TODO - envelope divider
+        pulse2.volume = data and 0x0F
       }
 
       REG_SQ2_SWEEP -> {
-
+        // TODO
       }
 
       REG_SQ2_LO -> {
-
+        pulse2.timer = (pulse2.timer and 0x0700) or data
       }
 
       REG_SQ2_HI -> {
-
+        pulse2.timer = (pulse2.timer and 0x00FF) or ((data and 0x07) shl 8)
+        pulse2.length = (data and 0xF8) ushr 5
       }
 
       REG_TRI_LINEAR -> {
+        // TODO - control flag
         triangle.linear = data and 0x7F
       }
 
@@ -55,35 +68,35 @@ class Apu(
       }
 
       REG_NOISE_VOL -> {
-
+        // TODO
       }
 
       REG_NOISE_LO -> {
-
+        // TODO
       }
 
       REG_NOISE_HI -> {
-
+        // TODO
       }
 
       REG_DMC_FREQ -> {
-
+        // TODO
       }
 
       REG_DMC_RAW -> {
-
+        // TODO
       }
 
       REG_DMC_START -> {
-
+        // TODO
       }
 
       REG_DMC_LEN -> {
-
+        // TODO
       }
 
       REG_SND_CHN -> {
-
+        // TODO
       }
 
       REG_FRAME_COUNTER_CTRL -> {
@@ -93,9 +106,15 @@ class Apu(
   }
 
   fun generate() {
-    val samples = triangle.generate(buffer.size)
+    val samplesPulse1 = pulse1.generate(buffer.size)
+    val samplesPulse2 = pulse2.generate(buffer.size)
+    val samplesTriangle = triangle.generate(buffer.size)
     for (i in buffer.indices) {
-      buffer[i] = (samples[i] * 10).toByte()
+      buffer[i] = ((
+        samplesPulse1[i] * 0.00752 +
+          samplesPulse2[i] * 0.00752 +
+          samplesTriangle[i] * 0.00851
+        ) * 100).toByte()
     }
   }
 
--- src/main/kotlin/choliver/nespot/apu/PulseGenerator.kt
+++ src/main/kotlin/choliver/nespot/apu/PulseGenerator.kt
@@ -2,27 +2,20 @@ package choliver.nespot.apu
 
 import kotlin.math.max
 
-// See http://wiki.nesdev.com/w/index.php/APU_Triangle
-class TriangleGenerator {
+// See http://wiki.nesdev.com/w/index.php/APU_Pulse
+class PulseGenerator {
   private val sequencer = Sequencer()
   private val timerCounter = Counter()
   private var iSeq = 0
-  private var iLinear = 0
   private var iLength = 0
-  private var linearY = 0   // TODO - needs a better name
 
-  var linear: Int = 0
-    set(value) {
-      field = value
-      linearY = value
-      iLinear = value
-    }
+  var dutyCycle = 0
+  var volume = 0
 
   var length: Int = 0
     set(value) {
       field = value
       iLength = LENGTH_TABLE[value]
-      iLinear = linearY   // Resets the linear counter too
     }
 
   var timer: Int = 0
@@ -31,33 +24,32 @@ class TriangleGenerator {
       timerCounter.periodCpuCycles = (value + 1).toDouble()
     }
 
-
   fun generate(num: Int) = List(num) {
     updateCounters()
     updatePhase()
-    SEQUENCE[iSeq]
+    SEQUENCES[dutyCycle][iSeq] * volume
   }
 
   private fun updateCounters() {
     val ticks = sequencer.update()
-    iLinear = max(iLinear - ticks.quarter, 0)
     iLength = max(iLength - ticks.half, 0)
   }
 
   private fun updatePhase() {
     val ticks = timerCounter.update()
-    if ((iLinear != 0) && (iLength != 0)) {
+    if (iLength != 0) {
       iSeq = (iSeq + ticks) % SEQUENCE_LENGTH
     }
   }
 
   companion object {
-    // TODO - what is the offset here?  Does it matter if there's a DC blocking filter?
-    private val SEQUENCE = listOf(
-      15, 14, 13, 12, 11, 10,  9,  8,  7,  6,  5,  4,  3,  2,  1,  0,
-      0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15
+    private val SEQUENCES = listOf(
+      listOf(0, 1, 0, 0, 0, 0, 0, 0),
+      listOf(0, 1, 1, 0, 0, 0, 0, 0),
+      listOf(0, 1, 1, 1, 1, 0, 0, 0),
+      listOf(1, 0, 0, 1, 1, 1, 1, 1)
     )
 
-    private const val SEQUENCE_LENGTH = 32
+    private const val SEQUENCE_LENGTH = 8
   }
 }

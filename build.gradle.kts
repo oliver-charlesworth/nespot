640f4c61791c15517863dada6f044d7f2bdaa078
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-04-25 13:23:26 +0100

    Remove deps (#43)
    * Remove redundant deps
    
    * Fix formatting
    
    * Bump Kotlin to 1.3.72

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   application
-  id("nebula.kotlin") version "1.3.61"
+  id("nebula.kotlin") version "1.3.72"
   id("org.openjfx.javafxplugin") version "0.0.8"
 }
 
@@ -12,14 +12,11 @@ repositories {
 }
 
 dependencies {
-  implementation("io.github.microutils:kotlin-logging:1.7.7")
   implementation("com.github.ajalt:clikt:2.6.0")
 
   testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
   testImplementation("org.hamcrest:hamcrest-library:2.2")
   testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
-
-  runtimeOnly("ch.qos.logback:logback-classic:1.2.3")
 }
 
 javafx {

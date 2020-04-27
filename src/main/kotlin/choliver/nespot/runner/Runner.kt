28af92119be02790835be22cf8e86204594f865e
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-04-27 22:24:01 +0100

    Implement full-screen mode

--- src/main/kotlin/choliver/nespot/runner/Runner.kt
+++ src/main/kotlin/choliver/nespot/runner/Runner.kt
@@ -3,6 +3,7 @@ package choliver.nespot.runner
 import choliver.nespot.nes.Nes
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.file
 import com.github.ajalt.clikt.parameters.types.int
@@ -14,53 +15,60 @@ import kotlin.system.measureTimeMillis
 class Runner : CliktCommand() {
   private val rom by argument().file(mustExist = true, canBeDir = false)
   private val numPerfFrames by option("--perf", "-p").int()
-
-  private val isClosed = AtomicBoolean(false)
-  private val joypads = FakeJoypads()
-  private val screen = Screen(
-    onButtonDown = { joypads.down(1, it) },
-    onButtonUp = { joypads.up(1, it) },
-    onClose = { isClosed.set(true) }
-  )
-  private val audio = Audio(frameRateHz = 60)   // TODO - move to a constant somewhere
+  private val isFullScreen by option("--fullscreen", "-f").flag()
 
   override fun run() {
-    val nes = Nes(
+    Inner().run()
+  }
+
+  inner class Inner {
+    private val isClosed = AtomicBoolean(false)
+    private val joypads = FakeJoypads()
+    private val screen = Screen(
+      isFullScreen = isFullScreen,
+      onButtonDown = { joypads.down(1, it) },
+      onButtonUp = { joypads.up(1, it) },
+      onClose = { isClosed.set(true) }
+    )
+    private val audio = Audio(frameRateHz = 60)   // TODO - move to a constant somewhere
+    private val nes = Nes(
       rom = rom.readBytes(),
       videoBuffer = screen.buffer,
       audioBuffer = audio.buffer,
       joypads = joypads
     )
-    nes.inspection.fireReset()
 
-    if (numPerfFrames == null) {
-      runNormally(nes)
-    } else {
-      runPerfTest(nes)
-    }
-  }
+    fun run() {
+      nes.inspection.fireReset()
 
-  private fun runNormally(nes: Nes) {
-    screen.show()
-    audio.start()
-
-    while (!isClosed.get()) {
-      measureNanoTime {
-        nes.runToEndOfFrame()
-        screen.redraw()
-        audio.play()
+      if (numPerfFrames == null) {
+        runNormally(nes)
+      } else {
+        runPerfTest(nes)
       }
     }
 
-    screen.exit()
-  }
+    private fun runNormally(nes: Nes) {
+      screen.show()
+      audio.start()
 
-  private fun runPerfTest(nes: Nes) {
-    val runtimeMs = measureTimeMillis {
-      repeat(numPerfFrames!!) { nes.runToEndOfFrame() }
+      while (!isClosed.get()) {
+        measureNanoTime {
+          nes.runToEndOfFrame()
+          screen.redraw()
+          audio.play()
+        }
+      }
+
+      screen.exit()
     }
 
-    println("Ran ${numPerfFrames!!} frames in ${runtimeMs} ms (${(numPerfFrames!! * 1000.0 / runtimeMs).roundToInt()} fps)")
+    private fun runPerfTest(nes: Nes) {
+      val runtimeMs = measureTimeMillis {
+        repeat(numPerfFrames!!) { nes.runToEndOfFrame() }
+      }
+      println("Ran ${numPerfFrames!!} frames in ${runtimeMs} ms (${(numPerfFrames!! * 1000.0 / runtimeMs).roundToInt()} fps)")
+    }
   }
 }
 

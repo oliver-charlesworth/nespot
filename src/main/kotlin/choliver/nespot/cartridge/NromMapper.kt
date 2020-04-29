a2a88ff083de2b8ec817394230d8052d5427b12a
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-04-29 21:19:28 +0100

    Tidy up

--- src/main/kotlin/choliver/nespot/cartridge/NromMapper.kt
+++ src/main/kotlin/choliver/nespot/cartridge/NromMapper.kt
@@ -17,46 +17,27 @@ class NromMapper(private val config: MapperConfig) : Mapper {
   }
 
   override val chr = object : ChrMemory {
-    // Separate implementations so we're not performing the conditional in the hot path
-    override fun intercept(ram: Memory) = when (config.mirroring) {
-      HORIZONTAL -> HorizontalChr(ram)
-      VERTICAL -> VerticalChr(ram)
+    val mapToVram: (Address) -> Address = when (config.mirroring) {
+      HORIZONTAL -> { addr -> (addr and 1023) or ((addr and 2048) shr 1) }
+      VERTICAL -> { addr -> addr and 2047 }
       IGNORED -> throw UnsupportedOperationException()
     }
-  }
 
-  private inner class HorizontalChr(private val ram: Memory) : Memory {
-    override fun load(addr: Address) = if (addr >= BASE_VRAM) {
-      ram.load(mapToVram(addr)) // This maps everything >= 0x4000 too
-    } else {
-      config.chrData[addr].data()
-    }
+    // Separate implementations so we're not performing the conditional in the hot path
+    override fun intercept(ram: Memory) = object : Memory {
+      override fun load(addr: Address) = if (addr >= BASE_VRAM) {
+        ram.load(mapToVram(addr)) // This maps everything >= 0x4000 too
+      } else {
+        config.chrData[addr].data()
+      }
 
-    override fun store(addr: Address, data: Data) {
-      // This maps everything >= 0x4000 too
-      if (addr >= BASE_VRAM) {
-        ram.store(mapToVram(addr), data)
+      override fun store(addr: Address, data: Data) {
+        // This maps everything >= 0x4000 too
+        if (addr >= BASE_VRAM) {
+          ram.store(mapToVram(addr), data)
+        }
       }
     }
-
-    private fun mapToVram(addr: Address) = (addr and 1023) or ((addr and 2048) shr 1)
-  }
-
-  private inner class VerticalChr(private val ram: Memory) : Memory {
-    override fun load(addr: Address) = if (addr >= BASE_VRAM) {
-      ram.load(mapToVram(addr)) // This maps everything >= 0x4000 too
-    } else {
-      config.chrData[addr].data()
-    }
-
-    override fun store(addr: Address, data: Data) {
-      // This maps everything >= 0x4000 too
-      if (addr >= BASE_VRAM) {
-        ram.store(mapToVram(addr), data)
-      }
-    }
-
-    private fun mapToVram(addr: Address) = addr and 2047
   }
 
   @Suppress("unused")

14021c8c3e28edc53ef32063cd893fae9011b27f
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-05-02 11:04:14 +0100

    Generate audio samples throughout scanline

--- src/main/kotlin/choliver/nespot/nes/Orchestrator.kt
+++ src/main/kotlin/choliver/nespot/nes/Orchestrator.kt
@@ -1,7 +1,7 @@
 package choliver.nespot.nes
 
+import choliver.nespot.CYCLES_PER_SAMPLE
 import choliver.nespot.CYCLES_PER_SCANLINE
-import choliver.nespot.SAMPLES_PER_SCANLINE
 import choliver.nespot.SCANLINES_PER_FRAME
 import choliver.nespot.apu.Apu
 import choliver.nespot.ppu.Ppu
@@ -13,15 +13,23 @@ internal class Orchestrator(
   private val ppu: Ppu
 ) {
   private var cyclesRemainingInScanline = CYCLES_PER_SCANLINE
-  private var samplesRemainingInScanline = SAMPLES_PER_SCANLINE
   private var scanlinesRemainingInFrame = SCANLINES_PER_FRAME
+  private var cyclesTilNextSample = CYCLES_PER_SAMPLE
   private var _endOfFrame = false
 
   val endOfFrame get() = _endOfFrame
 
   fun step() {
     _endOfFrame = false
-    cyclesRemainingInScanline -= cpu.executeStep()
+    val cycles = cpu.executeStep()
+    cyclesRemainingInScanline -= cycles
+    cyclesTilNextSample -= cycles
+
+    if (cyclesTilNextSample <= 0) {
+      apu.generateSample()
+      cyclesTilNextSample += CYCLES_PER_SAMPLE
+    }
+
     if (cyclesRemainingInScanline <= 0) {
       finishScanline()
     }
@@ -30,7 +38,6 @@ internal class Orchestrator(
   private fun finishScanline() {
     cyclesRemainingInScanline += CYCLES_PER_SCANLINE
     ppu.executeScanline()
-    generateSamplesForScanline()
     scanlinesRemainingInFrame--
     if (scanlinesRemainingInFrame == 0) {
       finishFrame()
@@ -43,13 +50,5 @@ internal class Orchestrator(
     }
     scanlinesRemainingInFrame = SCANLINES_PER_FRAME
     _endOfFrame = true
-  }
-
-  private fun generateSamplesForScanline() {
-    while (samplesRemainingInScanline > 0) {
-      apu.generateSample()
-      samplesRemainingInScanline -= 1
-    }
-    samplesRemainingInScanline += SAMPLES_PER_SCANLINE
   }
 }

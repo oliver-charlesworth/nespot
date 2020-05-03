99d8c7b3589a0cda78ba44008d719560205b7dcb
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-05-03 19:40:26 +0100

    Tidy

--- src/main/kotlin/choliver/nespot/cartridge/mappers/Mmc3Mapper.kt
+++ src/main/kotlin/choliver/nespot/cartridge/mappers/Mmc3Mapper.kt
@@ -36,17 +36,29 @@ class Mmc3Mapper(private val rom: Rom) : Mapper {
     override fun set(addr: Address, data: Data) {
       val even = (addr % 2) == 0
       when {
-        (addr >= BASE_REG_IRQ_ENABLE && !even) -> Unit  // TODO
-        (addr >= BASE_REG_IRQ_DISABLE && even) -> Unit  // TODO
-        (addr >= BASE_REG_IRQ_RELOAD && !even) -> Unit  // TODO
-        (addr >= BASE_REG_IRQ_LATCH && even) -> Unit  // TODO
-        (addr >= BASE_REG_PRG_RAM_PROTECT && !even) -> Unit  // TODO
-        (addr >= BASE_REG_MIRRORING && even) -> mirrorModeFlag = data.isBitSet(0)
-        (addr >= BASE_REG_BANK_DATA && !even) -> regs[regSelect] = data
-        (addr >= BASE_REG_BANK_SELECT && even) -> {
-          chrModeFlag = data.isBitSet(7)
-          prgModeFlag = data.isBitSet(6)
-          regSelect = data and 0b00000111
+        (addr >= BASE_PRG_ROM) -> {
+          when ((addr and 0x6000) shr 13) {
+            0 -> when (even) {
+              false -> regs[regSelect] = data
+              true -> {
+                chrModeFlag = data.isBitSet(7)
+                prgModeFlag = data.isBitSet(6)
+                regSelect = data and 0b00000111
+              }
+            }
+            1 -> when (even) {
+              false -> Unit // TODO - PRG-RAM protect
+              true -> mirrorModeFlag = data.isBitSet(0)
+            }
+            2 -> when (even) {
+              false -> Unit // TODO - IRQ reload
+              true -> Unit // TODO - IRQ latch
+            }
+            3 -> when (even) {
+              false -> Unit // TODO - IRQ enable
+              true -> Unit // TODO - IRQ disable
+            }
+          }
         }
         (addr >= BASE_PRG_RAM) -> prgRam[addr and 0x1FFF] = data
       }
@@ -89,16 +101,7 @@ class Mmc3Mapper(private val rom: Rom) : Mapper {
   companion object {
     const val BASE_PRG_RAM = 0x6000
     const val BASE_PRG_ROM = 0x8000
-
-    // Register ranges
-    const val BASE_REG_BANK_SELECT = 0x8000
-    const val BASE_REG_BANK_DATA = 0x8001
-    const val BASE_REG_MIRRORING = 0xA000
-    const val BASE_REG_PRG_RAM_PROTECT = 0xA001
-    const val BASE_REG_IRQ_LATCH = 0xC000
-    const val BASE_REG_IRQ_RELOAD = 0xC001
-    const val BASE_REG_IRQ_DISABLE = 0xE000
-    const val BASE_REG_IRQ_ENABLE = 0xE001
+    const val BASE_REG = 0x8000
 
     const val PRG_BANK_SIZE = 0x2000
     const val CHR_BANK_SIZE = 0x0400

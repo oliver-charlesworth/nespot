fef25e7b67b93c250e6aff1174599f8412e1e858
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-05-04 21:45:41 +0100

    Add CHR-ROM test cases

--- src/test/kotlin/choliver/nespot/cartridge/mappers/Mmc3MapperTest.kt
+++ src/test/kotlin/choliver/nespot/cartridge/mappers/Mmc3MapperTest.kt
@@ -3,8 +3,10 @@ package choliver.nespot.cartridge.mappers
 import choliver.nespot.Address
 import choliver.nespot.Data
 import choliver.nespot.Memory
+import choliver.nespot.cartridge.Mapper
 import choliver.nespot.cartridge.Rom
 import choliver.nespot.cartridge.mappers.Mmc3Mapper.Companion.BASE_PRG_ROM
+import choliver.nespot.cartridge.mappers.Mmc3Mapper.Companion.CHR_BANK_SIZE
 import choliver.nespot.cartridge.mappers.Mmc3Mapper.Companion.PRG_BANK_SIZE
 import choliver.nespot.data
 import com.nhaarman.mockitokotlin2.doReturn
@@ -16,6 +18,7 @@ import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
+
 
 class Mmc3MapperTest {
   @Nested
@@ -38,60 +41,25 @@ class Mmc3MapperTest {
     private val mapper = Mmc3Mapper(Rom(prgData = prgData))
 
     @Test
-    fun `bank 0 variable`() {
-      setModeAndReg(mode = 0, reg = 6, data = 3)
+    fun `mode 0`() {
+      mapper.setModeAndReg(prgMode = 0, reg = 6, data = 3)
+      mapper.setModeAndReg(prgMode = 0, reg = 7, data = 5)
 
       assertBankMapping(romBank = 3, prgBank = 0)
+      assertBankMapping(romBank = 5, prgBank = 1)
+      assertBankMapping(romBank = 6, prgBank = 2)   // Fixed to penultimate
+      assertBankMapping(romBank = 7, prgBank = 3)   // Fixed to last
     }
 
     @Test
-    fun `bank 0 fixed to penultimate`() {
-      setMode(mode = 1)
+    fun `mode 1`() {
+      mapper.setModeAndReg(prgMode = 1, reg = 6, data = 3)
+      mapper.setModeAndReg(prgMode = 1, reg = 7, data = 5)
 
-      assertBankMapping(romBank = 6, prgBank = 0)
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = [0, 1])
-    fun `bank 1 always variable`(mode: Int) {
-      setModeAndReg(mode = mode, reg = 7, data = 3)
-
-      assertBankMapping(romBank = 3, prgBank = 1)
-    }
-
-    @Test
-    fun `bank 2 fixed to penultimate`() {
-      setMode(mode = 0)
-
-      assertBankMapping(romBank = 6, prgBank = 2)
-    }
-
-    @Test
-    fun `bank 2 variable`() {
-      setModeAndReg(mode = 1, reg = 6, data = 3)
-
+      assertBankMapping(romBank = 6, prgBank = 0)   // Fixed to penultimate
+      assertBankMapping(romBank = 5, prgBank = 1)
       assertBankMapping(romBank = 3, prgBank = 2)
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = [0, 1])
-    fun `bank 3 always fixed to last`(mode: Int) {
-      setMode(mode = mode)
-
-      assertBankMapping(romBank = 7, prgBank = 3)
-    }
-
-    private fun setData(data: Map<Address, Data>) {
-      data.forEach { (addr, data) -> prgData[addr] = data.toByte() }
-    }
-
-    private fun setMode(mode: Int) {
-      mapper.prg[0x8000] = (mode shl 6)
-    }
-
-    private fun setModeAndReg(mode: Int, reg: Int, data: Data) {
-      mapper.prg[0x8000] = (mode shl 6) or reg
-      mapper.prg[0x8001] = data
+      assertBankMapping(romBank = 7, prgBank = 3)   // Fixed to last
     }
 
     // Test top and bottom of bank
@@ -100,16 +68,71 @@ class Mmc3MapperTest {
       val prgBase = BASE_PRG_ROM + (prgBank * PRG_BANK_SIZE)
       val offsetLast = PRG_BANK_SIZE - 1
 
-      setData(mapOf(
-        romBase to 0x30,
-        romBase + offsetLast to 0x40
-      ))
+      prgData[romBase] = 0x30
+      prgData[romBase + offsetLast] = 0x40
 
       assertEquals(0x30, mapper.prg[prgBase])
       assertEquals(0x40, mapper.prg[prgBase + offsetLast])
     }
   }
 
+  @Nested
+  inner class ChrRom {
+    private val chrData = ByteArray(8 * CHR_BANK_SIZE)
+    private val mapper = Mmc3Mapper(Rom(chrData = chrData))
+
+    @Test
+    fun `low banks are 2k, high banks are 1k`() {
+      mapper.setModeAndReg(chrMode = 0, reg = 0, data = 5) // Note we ignore LSB of selected bank
+      mapper.setModeAndReg(chrMode = 0, reg = 1, data = 0) // Note we ignore LSB of selected bank
+      mapper.setModeAndReg(chrMode = 0, reg = 2, data = 3)
+      mapper.setModeAndReg(chrMode = 0, reg = 3, data = 6)
+      mapper.setModeAndReg(chrMode = 0, reg = 4, data = 2)
+      mapper.setModeAndReg(chrMode = 0, reg = 5, data = 1)
+
+      assertBankMapping(romBank = 4, chrBank = 0)
+      assertBankMapping(romBank = 5, chrBank = 1)
+      assertBankMapping(romBank = 0, chrBank = 2)
+      assertBankMapping(romBank = 1, chrBank = 3)
+      assertBankMapping(romBank = 3, chrBank = 4)
+      assertBankMapping(romBank = 6, chrBank = 5)
+      assertBankMapping(romBank = 2, chrBank = 6)
+      assertBankMapping(romBank = 1, chrBank = 7)
+    }
+
+    @Test
+    fun `low banks are 1k, high banks are 2k`() {
+      mapper.setModeAndReg(chrMode = 1, reg = 0, data = 5) // Note we ignore LSB of selected bank
+      mapper.setModeAndReg(chrMode = 1, reg = 1, data = 0) // Note we ignore LSB of selected bank
+      mapper.setModeAndReg(chrMode = 1, reg = 2, data = 3)
+      mapper.setModeAndReg(chrMode = 1, reg = 3, data = 6)
+      mapper.setModeAndReg(chrMode = 1, reg = 4, data = 2)
+      mapper.setModeAndReg(chrMode = 1, reg = 5, data = 1)
+
+      assertBankMapping(romBank = 3, chrBank = 0)
+      assertBankMapping(romBank = 6, chrBank = 1)
+      assertBankMapping(romBank = 2, chrBank = 2)
+      assertBankMapping(romBank = 1, chrBank = 3)
+      assertBankMapping(romBank = 4, chrBank = 4)
+      assertBankMapping(romBank = 5, chrBank = 5)
+      assertBankMapping(romBank = 0, chrBank = 6)
+      assertBankMapping(romBank = 1, chrBank = 7)
+    }
+
+    // Test top and bottom of bank
+    private fun assertBankMapping(romBank: Int, chrBank: Int) {
+      val romBase = romBank * CHR_BANK_SIZE
+      val chrBase = chrBank * CHR_BANK_SIZE
+      val offsetLast = CHR_BANK_SIZE - 1
+
+      chrData[romBase] = 0x30
+      chrData[romBase + offsetLast] = 0x40
+
+      val chr = mapper.chr(mock())
+      assertEquals(0x30, chr[chrBase])
+      assertEquals(0x40, chr[chrBase + offsetLast])
+    }
+  }
 
   @Nested
   inner class Vram {
@@ -169,8 +192,6 @@ class Mmc3MapperTest {
     }
   }
 
-  // TODO - chr modes (all banks * 2)
-
   // TODO - irq asserted when countdown complete
 
   // TODO - irq cleared (statefully) by disable/enable loop
@@ -182,4 +203,10 @@ class Mmc3MapperTest {
   // TODO - reloads new value when reaches zero
 
   // TODO - reloads new value immediately if reload flag set
+
+
+  private fun Mapper.setModeAndReg(chrMode: Int = 0, prgMode: Int = 0, reg: Int, data: Data) {
+    prg[0x8000] = (chrMode shl 7) or (prgMode shl 6) or reg
+    prg[0x8001] = data
+  }
 }

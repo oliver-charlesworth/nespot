18801be39abdc289edb56e6077eb98dfa4c0b93f
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-05-08 21:17:23 +0100

    Make CYCLES_PER_SCANLINE accurate

--- src/main/kotlin/choliver/nespot/Constants.kt
+++ src/main/kotlin/choliver/nespot/Constants.kt
@@ -1,23 +1,16 @@
 package choliver.nespot
 
 const val SAMPLE_RATE_HZ = 44100
-const val FRAME_RATE_HZ = 60
 
 // See https://wiki.nesdev.com/w/index.php/Cycle_reference_chart
 val MASTER_FREQ_HZ = Rational(945e6.toInt(), 4) / 11
 val CPU_FREQ_HZ = MASTER_FREQ_HZ / 12
-val PPU_FREQ_HZ = MASTER_FREQ_HZ / 4
 
 // TODO - model skipping one dot every other frame
 const val DOTS_PER_SCANLINE = 341
 const val SCANLINES_PER_FRAME = 262
-
 val DOTS_PER_FRAME = Rational(DOTS_PER_SCANLINE * SCANLINES_PER_FRAME) - Rational(1, 2)
+val CYCLES_PER_FRAME = DOTS_PER_FRAME / 3
+val CYCLES_PER_SCANLINE = CYCLES_PER_FRAME / SCANLINES_PER_FRAME
 
 val CYCLES_PER_SAMPLE = CPU_FREQ_HZ / Rational(SAMPLE_RATE_HZ)
-
-val CYCLES_PER_FRAME = CPU_FREQ_HZ / FRAME_RATE_HZ
-const val SAMPLES_PER_FRAME = SAMPLE_RATE_HZ / FRAME_RATE_HZ
-
-val CYCLES_PER_SCANLINE = CYCLES_PER_FRAME / SCANLINES_PER_FRAME
-val SAMPLES_PER_SCANLINE = Rational(SAMPLES_PER_FRAME, SCANLINES_PER_FRAME)

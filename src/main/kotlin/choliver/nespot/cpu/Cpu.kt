c464d71691d73f96bb6dca257868f8c5afe87089
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-05-10 17:51:48 +0100

    More CPU cleanup

--- src/main/kotlin/choliver/nespot/cpu/Cpu.kt
+++ src/main/kotlin/choliver/nespot/cpu/Cpu.kt
@@ -6,6 +6,7 @@ import choliver.nespot.cpu.InstructionDecoder.Decoded
 import choliver.nespot.cpu.model.AddressMode.ACCUMULATOR
 import choliver.nespot.cpu.model.AddressMode.IMMEDIATE
 import choliver.nespot.cpu.model.Opcode.*
+import choliver.nespot.cpu.model.Regs
 import choliver.nespot.cpu.model.State
 import choliver.nespot.cpu.model.toFlags
 import choliver.nespot.cpu.utils._0
@@ -45,7 +46,7 @@ class Cpu(
   }
 
   private fun vector(addr: Address, updateStack: Boolean, disableIrq: Boolean): Int {
-    interrupt(addr, updateStack = updateStack, setBreakFlag = false)
+    state.regs.interrupt(addr, updateStack = updateStack, setBreakFlag = false)
     state.regs.p.i = state.regs.p.i || disableIrq
     return NUM_INTERRUPT_CYCLES
   }
@@ -54,189 +55,187 @@ class Cpu(
     decoder.decode(decoded, pc = state.regs.pc, x = state.regs.x, y = state.regs.y)
     state.regs.pc = decoded.nextPc
     extraCycles = 0
-    execute()
+    state.regs.execute()
     return decoded.numCycles + extraCycles
   }
 
-  private fun execute() {
-    state.regs.apply {
-      when (decoded.opcode) {
-        ADC -> add(resolve())
-        SBC -> add(resolve() xor 0xFF)
+  private fun Regs.execute() {
+    when (decoded.opcode) {
+      ADC -> add(resolve())
+      SBC -> add(resolve() xor 0xFF)
 
-        CMP -> compare(a, resolve())
-        CPX -> compare(x, resolve())
-        CPY -> compare(y, resolve())
+      CMP -> compare(a, resolve())
+      CPX -> compare(x, resolve())
+      CPY -> compare(y, resolve())
 
-        DEC -> storeResult(resolve() - 1)
-        DEX -> {
-          x = (x - 1).data()
-          updateZN(x)
-        }
-        DEY -> {
-          y = (y - 1).data()
-          updateZN(y)
-        }
-
-        INC -> storeResult(resolve() + 1)
-        INX -> {
-          x = (x + 1).data()
-          updateZN(x)
-        }
-        INY -> {
-          y = (y + 1).data()
-          updateZN(y)
-        }
-
-        ASL -> {
-          val data = resolve()
-          storeResult(data shl 1)
-          p.c = data.isBitSet(7)
-        }
-        LSR -> {
-          val data = resolve()
-          storeResult(data shr 1)
-          p.c = data.isBitSet(0)
-        }
-        ROL -> {
-          val data = resolve()
-          storeResult((data shl 1) or (if (p.c) 1 else 0))
-          p.c = data.isBitSet(7)
-        }
-        ROR -> {
-          val data = resolve()
-          storeResult((data shr 1) or (if (p.c) 0x80 else 0))
-          p.c = data.isBitSet(0)
-        }
-
-        AND -> {
-          a = a and resolve()
-          updateZN(a)
-        }
-        ORA -> {
-          a = a or resolve()
-          updateZN(a)
-        }
-        EOR -> {
-          a = a xor resolve()
-          updateZN(a)
-        }
-
-        BIT -> {
-          val data = resolve()
-          p.z = (a and data).isZero()
-          p.n = data.isNeg()
-          p.v = data.isBitSet(6)
-        }
-
-        LDA -> {
-          a = resolve()
-          updateZN(a)
-        }
-        LDX -> {
-          x = resolve()
-          updateZN(x)
-        }
-        LDY -> {
-          y = resolve()
-          updateZN(y)
-        }
-
-        STA -> memory[decoded.addr] = a
-        STX -> memory[decoded.addr] = x
-        STY -> memory[decoded.addr] = y
-
-        PHP -> push(p.data() or 0x10)  // Most online references state that PHP also sets B on stack
-        PHA -> push(a)
-        PLP -> p = pop().toFlags()
-        PLA -> {
-          a = pop()
-          updateZN(a)
-        }
-
-        JMP -> pc = decoded.addr
-
-        JSR -> {
-          // One before next instruction (note we already advanced PC)
-          push((pc - 1).hi())
-          push((pc - 1).lo())
-          pc = decoded.addr
-        }
-
-        RTS -> pc = (addr(lo = pop(), hi = pop()) + 1).addr()
-
-        RTI -> {
-          p = pop().toFlags()
-          pc = addr(lo = pop(), hi = pop())
-        }
-
-        BRK -> interrupt(VECTOR_IRQ, updateStack = true, setBreakFlag = true)
-
-        BPL -> branch(!p.n)
-        BMI -> branch(p.n)
-        BVC -> branch(!p.v)
-        BVS -> branch(p.v)
-        BCC -> branch(!p.c)
-        BCS -> branch(p.c)
-        BNE -> branch(!p.z)
-        BEQ -> branch(p.z)
-
-        TXA -> {
-          a = x
-          updateZN(a)
-        }
-        TYA -> {
-          a = y
-          updateZN(a)
-        }
-        TXS -> s = x
-        TAY -> {
-          y = a
-          updateZN(y)
-        }
-        TAX -> {
-          x = a
-          updateZN(x)
-        }
-        TSX -> {
-          x = s
-          updateZN(x)
-        }
-
-        CLC -> p.c = _0
-        CLD -> p.d = _0
-        CLI -> p.i = _0
-        CLV -> p.v = _0
-        SEC -> p.c = _1
-        SED -> p.d = _1
-        SEI -> p.i = _1
-
-        NOP -> { }
+      DEC -> storeResult(resolve() - 1)
+      DEX -> {
+        x = (x - 1).data()
+        updateZN(x)
       }
+      DEY -> {
+        y = (y - 1).data()
+        updateZN(y)
+      }
+
+      INC -> storeResult(resolve() + 1)
+      INX -> {
+        x = (x + 1).data()
+        updateZN(x)
+      }
+      INY -> {
+        y = (y + 1).data()
+        updateZN(y)
+      }
+
+      ASL -> {
+        val data = resolve()
+        storeResult(data shl 1)
+        p.c = data.isBitSet(7)
+      }
+      LSR -> {
+        val data = resolve()
+        storeResult(data shr 1)
+        p.c = data.isBitSet(0)
+      }
+      ROL -> {
+        val data = resolve()
+        storeResult((data shl 1) or (if (p.c) 1 else 0))
+        p.c = data.isBitSet(7)
+      }
+      ROR -> {
+        val data = resolve()
+        storeResult((data shr 1) or (if (p.c) 0x80 else 0))
+        p.c = data.isBitSet(0)
+      }
+
+      AND -> {
+        a = a and resolve()
+        updateZN(a)
+      }
+      ORA -> {
+        a = a or resolve()
+        updateZN(a)
+      }
+      EOR -> {
+        a = a xor resolve()
+        updateZN(a)
+      }
+
+      BIT -> {
+        val data = resolve()
+        p.z = (a and data).isZero()
+        p.n = data.isNeg()
+        p.v = data.isBitSet(6)
+      }
+
+      LDA -> {
+        a = resolve()
+        updateZN(a)
+      }
+      LDX -> {
+        x = resolve()
+        updateZN(x)
+      }
+      LDY -> {
+        y = resolve()
+        updateZN(y)
+      }
+
+      STA -> memory[decoded.addr] = a
+      STX -> memory[decoded.addr] = x
+      STY -> memory[decoded.addr] = y
+
+      PHP -> push(p.data() or 0x10)  // Most online references state that PHP also sets B on stack
+      PHA -> push(a)
+      PLP -> p = pop().toFlags()
+      PLA -> {
+        a = pop()
+        updateZN(a)
+      }
+
+      JMP -> pc = decoded.addr
+
+      JSR -> {
+        // One before next instruction (note we already advanced PC)
+        push((pc - 1).hi())
+        push((pc - 1).lo())
+        pc = decoded.addr
+      }
+
+      RTS -> pc = (addr(lo = pop(), hi = pop()) + 1).addr()
+
+      RTI -> {
+        p = pop().toFlags()
+        pc = addr(lo = pop(), hi = pop())
+      }
+
+      BRK -> interrupt(VECTOR_IRQ, updateStack = true, setBreakFlag = true)
+
+      BPL -> branch(!p.n)
+      BMI -> branch(p.n)
+      BVC -> branch(!p.v)
+      BVS -> branch(p.v)
+      BCC -> branch(!p.c)
+      BCS -> branch(p.c)
+      BNE -> branch(!p.z)
+      BEQ -> branch(p.z)
+
+      TXA -> {
+        a = x
+        updateZN(a)
+      }
+      TYA -> {
+        a = y
+        updateZN(a)
+      }
+      TXS -> s = x
+      TAY -> {
+        y = a
+        updateZN(y)
+      }
+      TAX -> {
+        x = a
+        updateZN(x)
+      }
+      TSX -> {
+        x = s
+        updateZN(x)
+      }
+
+      CLC -> p.c = _0
+      CLD -> p.d = _0
+      CLI -> p.i = _0
+      CLV -> p.v = _0
+      SEC -> p.c = _1
+      SED -> p.d = _1
+      SEI -> p.i = _1
+
+      NOP -> { }
     }
   }
 
-  private fun branch(cond: Boolean) {
+  private fun Regs.branch(cond: Boolean) {
     if (cond) {
       extraCycles++
-      if (((state.regs.pc xor decoded.addr) and 0xFF00) != 0) {
+      if (((pc xor decoded.addr) and 0xFF00) != 0) {
         extraCycles++   // Page change
       }
-      state.regs.pc = decoded.addr
+      pc = decoded.addr
     }
   }
 
-  private fun push(data: Data) {
-    memory[state.regs.s or 0x100] = data
-    state.regs.s = (state.regs.s - 1).data()
+  private fun Regs.push(data: Data) {
+    memory[s or 0x100] = data
+    s = (s - 1).data()
   }
 
-  private fun pop(): Data {
-    state.regs.s = (state.regs.s + 1).data()
-    return memory[state.regs.s or 0x100]
+  private fun Regs.pop(): Data {
+    s = (s + 1).data()
+    return memory[s or 0x100]
   }
 
-  private fun add(rhs: Data) = state.regs.apply {
+  private fun Regs.add(rhs: Data) {
     val c = p.c
     val raw = a + rhs + (if (c) 1 else 0)
     val result = raw.data()
@@ -249,7 +248,7 @@ class Cpu(
     updateZN(result)
   }
 
-  private fun compare(lhs: Data, rhs: Data) = state.regs.apply {
+  private fun Regs.compare(lhs: Data, rhs: Data) {
     val raw = (lhs + (rhs xor 0xFF) + 1)
     val result = raw.data()
 
@@ -257,7 +256,7 @@ class Cpu(
     updateZN(result)
   }
 
-  private fun interrupt(vector: Address, updateStack: Boolean, setBreakFlag: Boolean) = state.regs.apply {
+  private fun Regs.interrupt(vector: Address, updateStack: Boolean, setBreakFlag: Boolean) {
     if (updateStack) {
       push(pc.hi())
       push(pc.lo())
@@ -276,19 +275,19 @@ class Cpu(
     else -> memory[decoded.addr]
   }
 
-  private fun storeResult(data: Data) {
+  private fun Regs.storeResult(data: Data) {
     val d = data.data()
     if (decoded.addressMode == ACCUMULATOR) {
-      state.regs.a = d
+      a = d
     } else {
       memory[decoded.addr] = d
     }
     updateZN(d)
   }
 
-  private fun updateZN(data: Data) {
-    state.regs.p.z = data.isZero()
-    state.regs.p.n = data.isNeg()
+  private fun Regs.updateZN(data: Data) {
+    p.z = data.isZero()
+    p.n = data.isNeg()
   }
 
   enum class NextStep {

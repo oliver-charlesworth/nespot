4047eaf9897b03d1b77c7574aae15a737a3da0a7
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-05-13 22:01:42 +0100

    Yeah

--- src/main/kotlin/choliver/nespot/runner/Screen.kt
+++ src/main/kotlin/choliver/nespot/runner/Screen.kt
@@ -2,13 +2,16 @@ package choliver.nespot.runner
 
 import choliver.nespot.ppu.SCREEN_HEIGHT
 import choliver.nespot.ppu.SCREEN_WIDTH
+import choliver.nespot.ppu.TILE_SIZE
 import choliver.nespot.runner.Event.*
 import java.awt.Dimension
 import java.awt.Graphics
+import java.awt.Image
 import java.awt.event.KeyAdapter
 import java.awt.event.KeyEvent
 import java.awt.event.WindowAdapter
 import java.awt.event.WindowEvent
+import java.awt.geom.AffineTransform
 import java.awt.image.BufferedImage
 import java.awt.image.DataBufferInt
 import java.nio.IntBuffer
@@ -56,14 +59,23 @@ class Screen(
 
   private fun start() {
     frame = JFrame()
-    frame.size = Dimension(
-      SCREEN_WIDTH,
-      SCREEN_HEIGHT
-    )
+    frame.size = Dimension(OVERALL_WIDTH, OVERALL_HEIGHT)
 
     frame.add(object : JPanel() {
+      private var prev = 0L
+
       override fun paintComponent(g: Graphics) {
-        g.drawImage(image, 0, 0, null)
+        val now = System.currentTimeMillis()
+        println("Frame time = ${now - prev} ms")
+        prev = now
+        g.drawImage(
+          image
+            .getSubimage(0, TILE_SIZE, SCREEN_WIDTH, SCREEN_HEIGHT - 2 * TILE_SIZE)
+            .getScaledInstance(OVERALL_WIDTH, OVERALL_HEIGHT, Image.SCALE_DEFAULT),
+          0,
+          0,
+          null
+        )
       }
     })
 
@@ -91,5 +103,9 @@ class Screen(
   companion object {
     private const val SCALE = 4.0
     private const val RATIO_STRETCH = (8.0 / 7.0)    // Evidence in forums, etc. that PAR is 8/7, and it looks good
+
+    private const val OVERALL_WIDTH = (SCREEN_WIDTH * SCALE * RATIO_STRETCH).toInt()
+    private const val OVERALL_HEIGHT = ((SCREEN_HEIGHT - 2 * TILE_SIZE) * SCALE).toInt()
+
   }
 }

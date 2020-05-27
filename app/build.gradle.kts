692743ec089e6dbe090f0740a94a543028664e03
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-05-27 21:30:17 +0100

    Avoid javafx plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,23 +1,19 @@
 plugins {
   kotlin("jvm")
-  id("org.openjfx.javafxplugin") version "0.0.8"
   application
 }
 
 dependencies {
   implementation(project(":"))
   implementation(kotlin("stdlib"))
+  implementation("org.openjfx:javafx-base:14:mac")
+  implementation("org.openjfx:javafx-graphics:14:mac")
   implementation("com.github.ajalt:clikt:2.6.0")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.0")
   implementation("net.java.jinput:jinput:2.0.9")
   runtimeOnly("net.java.jinput:jinput:2.0.9:natives-all")
 
   testImplementation(project(":common-test"))
-}
-
-javafx {
-  version = "14"
-  modules = listOf("javafx.graphics")
 }
 
 application {

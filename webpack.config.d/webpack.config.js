b4c32de7093f6005048a202b9490426f88d8ad51
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-05-27 21:47:53 +0100

    Prevent open

--- webpack.config.d/webpack.config.js
+++ webpack.config.d/webpack.config.js
@@ -2,7 +2,7 @@
 // See e.g. https://medium.com/@JakeXiao/window-is-undefined-in-umd-library-output-for-webpack4-858af1b881df
 config.output.globalObject = "typeof self !== 'undefined' ? self : this";
 
-// All unknown paths resolve to index.html (allows us to specify ROM name in path)
 if (config.devServer) {
-  config.devServer.historyApiFallback = true;
+  config.devServer.historyApiFallback = true;  // Resolve unknown to index.html (allows us to specify ROM name in path)
+  config.devServer.open = false;  // Very annoying
 }

0f4316ee29ec3a68840df859a4b2092f4ffa220d
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-05-27 19:25:19 +0100

    WIP

--- webpack.config.d/webpack.config.js
+++ webpack.config.d/webpack.config.js
@@ -1,2 +1,5 @@
+// Default config doesn't work for workers.
 // See e.g. https://medium.com/@JakeXiao/window-is-undefined-in-umd-library-output-for-webpack4-858af1b881df
 config.output.globalObject = "typeof self !== 'undefined' ? self : this";
+
+config.devServer.historyApiFallback = true;

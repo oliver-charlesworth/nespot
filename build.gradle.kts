1829fbcfdf452b585264737b1f6bfc7d21d916e8
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-05-27 21:44:26 +0100

    Flatten

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,7 +56,11 @@ kotlin {
 
     jvm().compilations["test"].defaultSourceSet {
       dependencies {
-        implementation(project(":common-test"))
+        implementation("org.junit.jupiter:junit-jupiter:5.5.2")
+        implementation("org.hamcrest:hamcrest-library:2.2")
+        implementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
+        // byte-buddy 1.9.10 (pulled in by Mockito) behaves badly with Java 13 - see https://github.com/mockk/mockk/issues/397
+        implementation("net.bytebuddy:byte-buddy:1.10.6")
       }
     }
 

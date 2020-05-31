a2480bd260f0e39cd17a6d63b7f45ccf3aacde95
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-05-31 02:35:25 +0100

    Converge

--- src/commonMain/kotlin/choliver/nespot/cartridge/ChrMemory.kt
+++ src/commonMain/kotlin/choliver/nespot/cartridge/ChrMemory.kt
@@ -2,7 +2,7 @@ package choliver.nespot.cartridge
 
 import choliver.nespot.*
 import choliver.nespot.cartridge.Rom.Mirroring
-import choliver.nespot.cartridge.Rom.Mirroring.IGNORED
+import choliver.nespot.cartridge.Rom.Mirroring.*
 
 class ChrMemory(
   private val raw: ByteArray,
@@ -13,18 +13,26 @@ class ChrMemory(
   val bankMap = IntArray(CHR_SIZE / bankSize) { it }
 
   override fun get(addr: Address) = when {
-    (addr >= BASE_VRAM) -> vram[vramAddr(mirroring, addr)]    // This maps everything >= 0x4000 too
+    (addr >= BASE_VRAM) -> vram[vmap(addr)]    // This maps everything >= 0x4000 too
     else -> raw[map(addr)]
   }.data()
 
   override fun set(addr: Address, data: Data) {
     when {
-      (addr >= BASE_VRAM) -> vram[vramAddr(mirroring, addr)] = data.toByte()   // This maps everything >= 0x4000 too
+      (addr >= BASE_VRAM) -> vram[vmap(addr)] = data.toByte()   // This maps everything >= 0x4000 too
       else -> raw[map(addr)] = data.toByte()
     }
   }
 
   private fun map(addr: Address) = (addr % bankSize) + bankMap[addr / bankSize] * bankSize
+
+  private fun vmap(addr: Address): Address = when (mirroring) {
+    FIXED_LOWER -> (addr % NAMETABLE_SIZE)
+    FIXED_UPPER -> (addr % NAMETABLE_SIZE) + NAMETABLE_SIZE
+    VERTICAL -> addr and 2047
+    HORIZONTAL -> (addr and 1023) or ((addr and 2048) shr 1)
+    IGNORED -> throw UnsupportedOperationException()
+  }
 
   companion object {
     private const val CHR_SIZE = 8192

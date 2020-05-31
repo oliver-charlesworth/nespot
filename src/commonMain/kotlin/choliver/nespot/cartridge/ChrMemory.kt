7710fe19488b4e40d26ee6b8fbd6dda89156497d
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-05-31 12:29:14 +0100

    Port ChrMemory to use BankMap

--- src/commonMain/kotlin/choliver/nespot/cartridge/ChrMemory.kt
+++ src/commonMain/kotlin/choliver/nespot/cartridge/ChrMemory.kt
@@ -6,34 +6,23 @@ import choliver.nespot.cartridge.Rom.Mirroring.*
 
 class ChrMemory(
   private val raw: ByteArray,
-  private val bankSize: Int = raw.size,
+  bankSize: Int = raw.size,
   var mirroring: Mirroring = IGNORED
 ) : Memory {
-  init {
-    if ((bankSize % PAGE_SIZE) != 0) {
-      throw IllegalArgumentException("Bank size ${bankSize} not a multiple of page size ${PAGE_SIZE}")
-    }
-  }
-
-  // We map at page granularity, rather than bank granularity
-  private val pagesPerBank = bankSize / PAGE_SIZE
-  private val pageMap = IntArray(NUM_PAGES) { it * PAGE_SIZE }
-  val bankMap = IntArray(CHR_SIZE / bankSize) { it }
+  val bankMap = BankMap(bankSize = bankSize, addressSpaceSize = CHR_SIZE)
   private val vram = ByteArray(VRAM_SIZE)
 
   override fun get(addr: Address) = when {
     (addr >= BASE_VRAM) -> vram[vmap(addr)]    // This maps everything >= 0x4000 too
-    else -> raw[map(addr)]
+    else -> raw[bankMap.map(addr)]
   }.data()
 
   override fun set(addr: Address, data: Data) {
     when {
       (addr >= BASE_VRAM) -> vram[vmap(addr)] = data.toByte()   // This maps everything >= 0x4000 too
-      else -> raw[map(addr)] = data.toByte()
+      else -> raw[bankMap.map(addr)] = data.toByte()
     }
   }
-
-  private fun map(addr: Address) = (addr % bankSize) + bankMap[addr / bankSize] * bankSize
 
   private fun vmap(addr: Address) = when (mirroring) {
     FIXED_LOWER -> (addr % NAMETABLE_SIZE)
@@ -41,10 +30,5 @@ class ChrMemory(
     VERTICAL -> addr and 2047
     HORIZONTAL -> (addr and 1023) or ((addr and 2048) shr 1)
     IGNORED -> throw UnsupportedOperationException()
-  }
-
-  companion object {
-    private const val PAGE_SIZE = 1024    // This should be no bigger than the minimum known bank size
-    private const val NUM_PAGES = CHR_SIZE / PAGE_SIZE
   }
 }

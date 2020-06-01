b9b215ea268dca135a414ac12270dfde1f625ca7
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-06-01 19:35:06 +0100

    Delete Mapper71

--- src/commonMain/kotlin/choliver/nespot/cartridge/Cartridge.kt
+++ src/commonMain/kotlin/choliver/nespot/cartridge/Cartridge.kt
@@ -49,7 +49,7 @@ class Cartridge(private val mapper: Mapper) {
           2 -> UxRomMapper(rom)
           3 -> CnRomMapper(rom)
           4 -> Mmc3Mapper(rom)
-          71 -> Mapper71(rom)
+          71 -> UxRomMapper(rom)  // Can re-use MMC1 (see https://wiki.nesdev.com/w/index.php/INES_Mapper_071)
           else -> throw RuntimeException("Mapper #${rom.mapper}")
         }
       )

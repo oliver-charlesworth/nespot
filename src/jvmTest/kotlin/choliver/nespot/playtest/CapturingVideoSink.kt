3a6f8849f1dbca4eb6f11fb587f23db7f0c79310
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-06-03 00:08:30 +0100

    WIP

--- src/jvmTest/kotlin/choliver/nespot/playtest/CapturingVideoSink.kt
+++ src/jvmTest/kotlin/choliver/nespot/playtest/CapturingVideoSink.kt
@@ -5,23 +5,22 @@ import choliver.nespot.SCREEN_WIDTH
 import choliver.nespot.VideoSink
 
 class CapturingVideoSink(private val delegate: VideoSink) : VideoSink {
-  private val bufferA = MutableList<Byte>(SCREEN_WIDTH * SCREEN_HEIGHT * 4) { 0 }
-  private val bufferB = MutableList<Byte>(SCREEN_WIDTH * SCREEN_HEIGHT * 4) { 0 }
-  private var buffer = bufferA
   private var idx = 0
-
-  val snapshot get() = (if (buffer === bufferA) bufferB else bufferA).toList()
+  private var buffer = mutableListOf<Byte>()
+  private var _snapshot = listOf<Byte>()
+  val snapshot get() = _snapshot
 
   override val colorPackingMode = delegate.colorPackingMode
 
   override fun put(color: Int) {
     delegate.put(color)
-    buffer[idx++] = (color shr 0).toByte()
-    buffer[idx++] = (color shr 8).toByte()
-    buffer[idx++] = (color shr 16).toByte()
-    buffer[idx++] = (color shr 24).toByte()
-    if (idx == buffer.size) {
-      buffer = if (buffer === bufferA) bufferB else bufferA
+    buffer.add((color shr 0).toByte())
+    buffer.add((color shr 8).toByte())
+    buffer.add((color shr 16).toByte())
+    buffer.add((color shr 24).toByte())
+    if (++idx == SCREEN_WIDTH * SCREEN_HEIGHT) {
+      _snapshot = buffer
+      buffer = mutableListOf()
       idx = 0
     }
   }

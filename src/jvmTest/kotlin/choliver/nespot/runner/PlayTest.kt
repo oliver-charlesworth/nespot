ef05b8207dcaba280d2917a904fef107d70cfac2
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-06-02 22:51:52 +0100

    awful

--- src/jvmTest/kotlin/choliver/nespot/runner/PlayTest.kt
+++ src/jvmTest/kotlin/choliver/nespot/runner/PlayTest.kt
@@ -1,58 +1,39 @@
 package choliver.nespot.runner
 
 import choliver.nespot.cartridge.Rom
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.io.File
-import java.util.zip.ZipInputStream
 
-//@Disabled("Intended to be run manually")
 class PlayTest {
+
+  // TODO - expectation that .nes file exists
   @Test
   fun `super mario bros`() = execute("smb")
 
   private fun execute(name: String) {
     if (RECORD) {
-      record(name)
+      val recording = runner(name).run()
+      serdes.serialiseTo(captureFile(name), recording)
     } else {
-//      compare(name)
+      val original = serdes.deserialiseFrom(captureFile(name))
+      val recording = runner(name, original).run()
+      assertEquals(original, recording)
     }
   }
-
-  private fun record(name: String) {
-    val scenario = runner(name).run()
-    Serdes().serialiseTo(captureFile(name), scenario)
-  }
-
-//  private fun compare(name: String) {
-//    val original = readFromZipFile<List<TimestampedEvent>>(name)
-//    original.snapshots().forEachIndexed { idx, s -> savePng("${idx}", s.data) }
-//    exitProcess(0)
-//    val recording = runner(name, original).run()
-//
-//    assertEquals(original.snapshots(), recording.snapshots())
-//  }
-
-  private inline fun <reified T : Any> readFromZipFile(name: String) =
-    ZipInputStream(captureFile(name).inputStream().buffered()).use { zis ->
-      zis.nextEntry
-      mapper.readValue<T>(zis)
-    }
 
   private fun runner(name: String, ghost: Scenario? = null) =
     CapturingRunner(Rom.parse(romFile(name).readBytes()), ghost)
 
   private fun captureFile(name: String) = File(CAPTURES_BASE, "${name}.zip")
-
   private fun romFile(name: String) = File(TEST_ROMS_BASE, "${name}.nes")
 
-  private val mapper = jacksonObjectMapper()
+  private val serdes = Serdes()
 
   companion object {
     private val CAPTURES_BASE = File("captures")
     private val TEST_ROMS_BASE = File("roms")
 
-    const val RECORD = true
+    const val RECORD = false
   }
 }
--- src/jvmTest/kotlin/choliver/nespot/runner/Serdes.kt
+++ src/jvmTest/kotlin/choliver/nespot/runner/Serdes.kt
@@ -4,35 +4,71 @@ import choliver.nespot.SCREEN_HEIGHT
 import choliver.nespot.SCREEN_WIDTH
 import choliver.nespot.runner.Scenario.Stimulus.*
 import choliver.nespot.sha1
+import com.fasterxml.jackson.core.JsonGenerator.Feature.AUTO_CLOSE_TARGET
+import com.fasterxml.jackson.core.JsonParser.Feature.AUTO_CLOSE_SOURCE
 import com.fasterxml.jackson.databind.SerializationFeature.INDENT_OUTPUT
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import java.awt.image.BufferedImage
 import java.awt.image.BufferedImage.TYPE_INT_RGB
 import java.io.File
 import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
 import java.util.zip.ZipOutputStream
 import javax.imageio.ImageIO
 
 class Serdes {
   private val mapper = jacksonObjectMapper()
     .enable(INDENT_OUTPUT)
+    .disable(AUTO_CLOSE_TARGET)
+    .disable(AUTO_CLOSE_SOURCE)
 
   fun serialiseTo(zip: File, scenario: Scenario) {
     val converted = convertScenario(scenario)
+    toZip(zip, converted)
+  }
 
-    // TODO - delete if fails partway through
+  fun deserialiseFrom(zip: File): Scenario {
+    val converted = fromZip(zip)
+    return unconvertScenario(converted)
+  }
+
+  private fun toZip(zip: File, converted: ScenarioAndSnapshots) {
     ZipOutputStream(zip.outputStream().buffered()).use { zos ->
       converted.snapshots.forEach { (hash, image) ->
         zos.putNextEntry(ZipEntry("${hash}.png"))
         ImageIO.write(image, "PNG", zos)
+        zos.closeEntry()
       }
 
-      zos.putNextEntry(ZipEntry("scenario.json"))
+      zos.putNextEntry(ZipEntry(SCENARIO_FILENAME))
       mapper.writeValue(zos, converted.scenario)
+      zos.closeEntry()
     }
   }
 
-  private fun convertScenario(scenario: Scenario): AllThings {
+  private fun fromZip(zip: File): ScenarioAndSnapshots {
+    val snapshots = mutableMapOf<String, BufferedImage>()
+    var scenario: SerialisedScenario? = null
+
+    ZipInputStream(zip.inputStream().buffered()).use { zis ->
+      loop@ while (true) {
+        when (val name = zis.nextEntry?.name) {
+          null -> break@loop
+          SCENARIO_FILENAME -> scenario = mapper.readValue(zis)
+          else -> snapshots[name.removeSuffix(".png")] = ImageIO.read(zis)
+        }
+      }
+    }
+
+    if (scenario != null) {
+      return ScenarioAndSnapshots(scenario!!, snapshots)
+    } else {
+      throw RuntimeException("Malformed scenario file")
+    }
+  }
+
+  private fun convertScenario(scenario: Scenario): ScenarioAndSnapshots {
     val snapshots = mutableMapOf<String, BufferedImage>()
     val converted = SerialisedScenario(
       scenario.romHash,
@@ -41,27 +77,55 @@ class Serdes {
           is ButtonDown -> SerialisedScenario.Stimulus.ButtonDown(s.timestamp, s.button)
           is ButtonUp -> SerialisedScenario.Stimulus.ButtonUp(s.timestamp, s.button)
           is Snapshot -> {
-            val snapshot = convertSnapshot(s)
-            snapshots[snapshot.hash] = snapshot.image
-            SerialisedScenario.Stimulus.Snapshot(s.timestamp, snapshot.hash)
+            val imageAndHash = convertImage(s.pixels)
+            snapshots[imageAndHash.hash] = imageAndHash.image
+            SerialisedScenario.Stimulus.Snapshot(s.timestamp, imageAndHash.hash)
           }
           is Close -> SerialisedScenario.Stimulus.Close(s.timestamp)
         }
       }
     )
 
-    return AllThings(
+    return ScenarioAndSnapshots(
       scenario = converted,
       snapshots = snapshots
     )
   }
 
-  private fun convertSnapshot(snapshot: Snapshot): Converted {
-    val raw = toRaw(snapshot)
-    return Converted(
+  private fun unconvertScenario(converted: ScenarioAndSnapshots): Scenario {
+    return Scenario(
+      converted.scenario.romHash,
+      converted.scenario.stimuli.map { s ->
+        when (s) {
+          is SerialisedScenario.Stimulus.ButtonDown -> ButtonDown(s.timestamp, s.button)
+          is SerialisedScenario.Stimulus.ButtonUp -> ButtonUp(s.timestamp, s.button)
+          is SerialisedScenario.Stimulus.Snapshot -> {
+            Snapshot(s.timestamp, unconvertImage(converted.snapshots[s.hash]!!))
+          }
+          is SerialisedScenario.Stimulus.Close -> Close(s.timestamp)
+        }
+      }
+    )
+  }
+
+  private fun convertImage(pixels: List<Int>): ImageAndHash {
+    val raw = toRaw(pixels)
+    return ImageAndHash(
       image = createImage(raw),
       hash = raw.toByteArray().sha1()
     )
+  }
+
+  private fun unconvertImage(image: BufferedImage): List<Int> {
+    val raw = MutableList<Byte>(SCREEN_WIDTH * SCREEN_HEIGHT * 4) { 0 }
+    for (i in 0 until SCREEN_WIDTH * SCREEN_HEIGHT) {
+      val pixel = image.getRGB(i % SCREEN_WIDTH, i / SCREEN_WIDTH)
+      raw[i * 4 + 0] = (pixel shr 24).toByte()
+      raw[i * 4 + 1] = (pixel shr 16).toByte()
+      raw[i * 4 + 2] = (pixel shr 8).toByte()
+      raw[i * 4 + 3] = (pixel shr 0).toByte()
+    }
+    return fromRaw(raw)
   }
 
   private fun createImage(raw: List<Byte>) =
@@ -76,17 +140,32 @@ class Serdes {
       }
     }
 
-  private fun toRaw(snapshot: Snapshot) = snapshot.pixels
+  private fun toRaw(pixels: List<Int>) = pixels
     .flatMap { listOf(it shr 0, it shr 8, it shr 16, it shr 24) }
     .map { it.toByte() }
 
-  private class AllThings(
+  private fun fromRaw(raw: List<Byte>) = raw
+    .map { it.toInt() }
+    .chunked(4)
+    .map {
+      0 +
+        ((it[0] shl 0) and 0xFF) +
+        ((it[1] shl 8) and 0xFF00) +
+        ((it[2] shl 16) and 0xFF0000) +
+        (it[3] shl 24)
+    }
+
+  private class ScenarioAndSnapshots(
     val scenario: SerialisedScenario,
     val snapshots: Map<String, BufferedImage>
   )
 
-  private class Converted(
+  private class ImageAndHash(
     val image: BufferedImage,
     val hash: String
   )
+
+  companion object {
+    private const val SCENARIO_FILENAME = "scenario.json"
+  }
 }

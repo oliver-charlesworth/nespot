74fe3abb57f23ae46e3aef51a4d9f3789f579428
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-06-06 18:05:57 +0100

    Inline

--- src/jvmMain/kotlin/choliver/nespot/runner/Cli.kt
+++ src/jvmMain/kotlin/choliver/nespot/runner/Cli.kt
@@ -1,13 +1,13 @@
 package choliver.nespot.runner
 
 import choliver.nespot.cartridge.Rom
+import choliver.nespot.printInfo
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.file
 import com.github.ajalt.clikt.parameters.types.int
-
 
 class Cli : CliktCommand(name = "nespot") {
   private val raw by argument(name = "rom").file(mustExist = true, canBeDir = false)
@@ -19,7 +19,7 @@ class Cli : CliktCommand(name = "nespot") {
     val rom = Rom.parse(raw.readBytes())
 
     when {
-      displayInfo -> InfoRunner(rom).run()
+      displayInfo -> rom.printInfo()
       (numPerfFrames != null) -> PerfRunner(rom, numPerfFrames!!).run()
       else -> InteractiveRunner(rom, fullScreen).run()
     }

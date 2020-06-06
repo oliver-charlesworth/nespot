429944688ad47730fa8cc1637f99c031ee2fff65
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-06-06 13:36:22 +0100

    Add tests

--- src/commonMain/kotlin/choliver/nespot/mappers/Mapper71.kt
+++ src/commonMain/kotlin/choliver/nespot/mappers/Mapper71.kt
@@ -23,6 +23,7 @@ class Mapper71(private val rom: Rom) : Mapper {
 
   override fun Cartridge.onPrgSet(addr: Address, data: Data) {
     when {
+      (addr >= BASE_CIC_STUN) -> Unit
       (addr >= BASE_BANK_SELECT) -> prg.bankMap[0] = data % numPrgBanks
       (addr >= BASE_MIRRORING) -> chr.mirroring = when ((data and 0x10) shr 4) {
         0 -> FIXED_LOWER
@@ -35,7 +36,9 @@ class Mapper71(private val rom: Rom) : Mapper {
   companion object {
     const val CHR_RAM_SIZE = 8192
     const val PRG_BANK_SIZE = 16384
+    // Based on the hack described here: https://wiki.nesdev.com/w/index.php/INES_Mapper_071#Mirroring_.28.248000-.249FFF.29
     const val BASE_MIRRORING = 0x9000
     const val BASE_BANK_SELECT = 0xC000
+    const val BASE_CIC_STUN = 0xE000
   }
 }
---
+++ src/jvmTest/kotlin/choliver/nespot/mappers/Mapper71Test.kt
@@ -0,0 +1,85 @@
+package choliver.nespot.mappers
+
+import choliver.nespot.cartridge.Cartridge
+import choliver.nespot.cartridge.Rom
+import choliver.nespot.cartridge.Rom.Mirroring.*
+import choliver.nespot.mappers.Mapper71.Companion.BASE_BANK_SELECT
+import choliver.nespot.mappers.Mapper71.Companion.BASE_MIRRORING
+import choliver.nespot.mappers.Mapper71.Companion.CHR_RAM_SIZE
+import choliver.nespot.mappers.Mapper71.Companion.PRG_BANK_SIZE
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.verifyNoMoreInteractions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.mockito.Answers.RETURNS_DEEP_STUBS
+
+class Mapper71Test {
+  private val cartridge = mock<Cartridge>(defaultAnswer = RETURNS_DEEP_STUBS)
+
+  @Test
+  fun `configures CHR RAM`() {
+    val mapper = Mapper71(Rom())
+
+    assertEquals(CHR_RAM_SIZE, mapper.chrData.size)
+  }
+
+  @Test
+  fun `fixes upper bank on startup`() {
+    with(Mapper71(Rom(prgData = ByteArray(8 * PRG_BANK_SIZE)))) {
+      cartridge.onStartup()
+    }
+
+    verify(cartridge.prg.bankMap)[1] = 7
+    verifyNoMoreInteractions(cartridge.prg.bankMap)
+  }
+
+  @Test
+  fun `dynamically sets lower bank`() {
+    with(Mapper71(Rom(prgData = ByteArray(8 * PRG_BANK_SIZE)))) {
+      cartridge.onPrgSet(BASE_BANK_SELECT, 6)
+
+      verify(cartridge.prg.bankMap)[0] = 6
+    }
+  }
+
+  @Test
+  fun `bank mapping wraps`() {
+    with(Mapper71(Rom(prgData = ByteArray(8 * PRG_BANK_SIZE)))) {
+      cartridge.onPrgSet(BASE_BANK_SELECT, 6 + 8)
+
+      verify(cartridge.prg.bankMap)[0] = 6
+    }
+  }
+
+  @Nested
+  inner class Mirroring {
+    @Test
+    fun `on startup`() {
+      with(Mapper71(Rom(mirroring = VERTICAL))) {
+        cartridge.onStartup()
+      }
+
+      verify(cartridge.chr).mirroring = VERTICAL
+    }
+
+    @Test
+    fun `dynamic - fixed-lower`() {
+      with(Mapper71(Rom())) {
+        cartridge.onPrgSet(BASE_MIRRORING, 0x00)
+
+        verify(cartridge.chr).mirroring = FIXED_LOWER
+      }
+    }
+
+    @Test
+    fun `dynamic - fixed-upper`() {
+      with(Mapper71(Rom())) {
+        cartridge.onPrgSet(BASE_MIRRORING, 0x10)
+
+        verify(cartridge.chr).mirroring = FIXED_UPPER
+      }
+    }
+  }
+}
